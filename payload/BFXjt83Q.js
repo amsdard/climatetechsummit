7ecc5ffe1a7678bf90cd7e194f594db10e9a5796
Jalokim <jalokimgraphics@gmail.com>
2024-09-03 14:49:53 +0200

    update 03.09

--- payload/BFXjt83Q.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./Cw6N_1qc.js";import{M as u}from"./CQFiQZXc.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
