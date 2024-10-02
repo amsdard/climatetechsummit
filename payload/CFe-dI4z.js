4daacdf6fb20cad58862d6ce31950a56d990aad4
Jalokim <jalokimgraphics@gmail.com>
2024-10-02 10:29:11 +0200

    update 02.10

--- payload/CFe-dI4z.js
+++
@@ -1,1 +0,0 @@
-import{u as s}from"./Cqm2QHkt.js";import{M as u}from"./DNGilsEE.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
