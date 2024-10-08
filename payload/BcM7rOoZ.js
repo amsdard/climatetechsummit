020adceecbbd7617ab14f29c85009b8c4efa9bf9
Jalokim <jalokimgraphics@gmail.com>
2024-10-08 16:48:58 +0200

    update 08.10

--- payload/BcM7rOoZ.js
+++
@@ -1,1 +0,0 @@
-import{j as c,o as e,c as s}from"./D_0PWFiG.js";const o={key:0,class:"h-0"},i={key:1,class:"h-5"},a={key:2,class:"h-10"},n={key:3,class:"h-20"},l=c({__name:"Whitespace",props:{size:{type:Number,default:3}},setup(t){return(_,d)=>t.size===0?(e(),s("div",o)):t.size===1?(e(),s("div",i)):t.size===2?(e(),s("div",a)):(e(),s("div",n))}});export{l as _};
---
+++ payload/jHFjmsGd.js
@@ -0,0 +1,1 @@
+import{u as s}from"./i2wUCYc9.js";import{M as u}from"./BmeN_UOs.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
