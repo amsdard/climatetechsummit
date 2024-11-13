2fe1a704c2bb8d90276a7ac6c3dc1c03040e8716
Jalokim <jalokimgraphics@gmail.com>
2024-11-13 13:41:07 +0100

    switch homepages

---
+++ payload/BqKoGUZ5.js
@@ -0,0 +1,1 @@
+import{u as s}from"./DkBH_4Pa.js";import{M as u}from"./BU2hMqdS.js";function M(e,a){const{title:r,titleTemplate:o,...m}=e;return s({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=u({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{M as u};
